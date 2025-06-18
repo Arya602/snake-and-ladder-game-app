bd08eecc6c4cb4480feac072a7fb6deaee6b7d57
Arya V R <arya.vr@ultsglobal.com>
2025-06-18 18:08:39 +0530

    Player Detail Form

--- src/components/Dashboard/PlayerSetup.jsx
+++ src/components/Dashboard/PlayerSetup.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { MdPersonPinCircle } from "react-icons/md";
 import {
     Grid,
@@ -7,40 +7,65 @@ import {
     Button,
     MenuItem,
     Select,
+    colors,
 } from "@mui/material";
+import './PlayerSetup.css'
+import cat from '../../assets/png/cat.png'
+import { motion } from 'framer-motion';
 
 const colorList = ["red", "yellow", "green", "blue"];
-const defaultIcon = <MdPersonPinCircle size={30} />;
+
 
 const PlayerSetup = () => {
     const [NoOfPlayers, setNoOfPlayers] = useState();
     const [playerList, setPlayerList] = useState([]);
     const [showForm, setShowForm] = useState(false);
+    const [colorError, setColorError] = useState('');
+    const [tokenColorList, setTokenColorList] = useState([])
+    const [playerImagesList, setPlayerImagesList] = useState([])
 
-    const handleStartGame = () => {
-        setShowForm(true);
-    };
+    const [image, setImage] = useState(null);
+
+    const handleStartGame = () => setShowForm(true);
 
     const handlePlayerNumberChange = (e) => {
         const count = parseInt(e.target.value, 10);
         setNoOfPlayers(count);
-        const Players = Array.from({ length: count }, (_, i) => ({
+        const Players = Array.from({ length: count }, () => ({
             name: '',
-            color: colorList[i],
-            icon: defaultIcon,
+            color: '',
+            // icon: defaultIcon,
         }));
         setPlayerList(Players);
     }
 
     const handlePlayerNameChange = (index, name) => {
+        console.log("player name", playerList);
+
         const playerListCopy = [...playerList];
+        console.log("player name", playerListCopy);
         playerListCopy[index].name = name;
         setPlayerList(playerListCopy);
+        console.log("playerListCopy", playerListCopy);
+
+    }
+
+    const handleTokenColor = (color, index) => {
+        const colorAlreadySelected = playerList.some((player, i) => player.color === color && i !== index);
+        if (colorAlreadySelected) {
+            console.log("coloorrrrrrr");
+
+            setColorError(`Color "${selectedColor}" already chosen by another player.`);
+            return;
+        }
+        const copyPlayerList = [...playerList];
+        copyPlayerList[index].color = color;
+        setPlayerList(copyPlayerList);
+
     }
 
     const handlePlay = () => {
-        if(NoOfPlayers && playerList.every(p => p.name.trim()))
-        {
+        if (NoOfPlayers && playerList.every(p => p.name.trim())) {
             onStartGame(playerList);
         }
         else {
@@ -48,45 +73,86 @@ const PlayerSetup = () => {
         }
     }
 
+    const isColorSelected = (color) =>
+        playerList.some(player => player.color === color);
+
     return (
         <div className="player-setup-page">
             <div className="overlay">
                 {!showForm ?
-                    (<Button className='start-game-btn' onClick={handleStartGame}>
-                        Start Game
-                    </Button>
+                    (
+                        <motion.div
+                            className='start-game-text'
+                            initial={{
+                                scale: 1,
+                                // textShadow: '0 0 0px rgba(0, 255, 0, 0)',
+                                // WebkitTextStroke: '2px #00ff00'
+                            }}
+                            whileHover={{
+                                scale: 1.1,
+                                // WebkitTextStroke: '3px #ff00ff',
+                                // textShadow: '0 0 10px rgba(255, 0, 255, 0.7)'
+                            }}
+                            transition={{ type: 'spring', stiffness: 400, damping: 10 }}
+                            onClick={handleStartGame}
+                        >Start Game</motion.div>
+                        // <Button className='start-game-btn' onClick={handleStartGame}>
+                        //     <h1>Start Game</h1>
+                        // </Button>
                     ) :
                     (
                         <div className="form-container">
                             <div className="form-group">
-                                <Typography>Select Numner of Players</Typography>
-                                <Select
-                                    fullWidth
-                                    variant="outlined"
+                                <select
+                                    className='no-of-player-dropdown'
                                     value={NoOfPlayers}
                                     placeholder='Select Number of Players'
                                     onChange={handlePlayerNumberChange}
                                 >
-                                    <MenuItem value="">Select</MenuItem>
+                                    <option value="">Select Numner of Players</option>
                                     {[1, 2, 3, 4].map(n => (
-                                        <MenuItem key={n} value={n}>{n}</MenuItem>
+                                        <option key={n} value={n}>{n}</option>
                                     ))}
-                                </Select>
-                                {playerList.map((player, index) => (
-                                    <div className="player-form">
-                                        <h3>Player {index + 1}</h3>
-                                        <input
-                                            type="text"
-                                            placeholder='Enter Player Name'
-                                            value={player.name}
-                                            onChange={(e) => handlePlayerNameChange(index, e.target.value)}
-                                        />
-                                        <div className="token-icon">
-                                            {player.icon}
-                                        </div>
-                                    </div>
-                                ))}
-                                <Button className='start-game-btn' onClick={handlePlay}>
+                                </select>
+                                <Grid container spacing={2}>
+                                    {playerList.map((player, index) => (
+                                        <Grid item xs={12} sm={6} key={index}>
+                                            <div className="player-form">
+                                                <img
+                                                    src={image || cat}
+                                                    alt="Player"
+                                                    style={{ width: '80px', height: '100px', borderRadius: '20px' }}
+                                                />
+                                                <input
+                                                    type="text"
+                                                    placeholder='Enter Player Name'
+                                                    value={player.name}
+                                                    onChange={(e) => handlePlayerNameChange(index, e.target.value)}
+                                                />
+                                                <div className="token-icon">
+                                                    {colorList.map((color, i) => (
+                                                        <span key={i}
+                                                            onClick={() => handleTokenColor(color, index)}
+                                                            style={{
+                                                                cursor: 'pointer',
+                                                                margin: '0px 3px',
+                                                                color: color,
+                                                                opacity: player.color === color ? 1 : 0.7,
+                                                                border: player.color === color ? 'black' : 'none',
+                                                                borderRadius: '100%',
+                                                                padding: '3px'
+                                                            }}
+                                                        >
+                                                            <MdPersonPinCircle size={32} />
+                                                        </span>
+                                                    ))}
+                                                </div>
+                                            </div>
+                                        </Grid>
+                                    ))}
+                                </Grid>
+
+                                <Button className='play-btn' onClick={handlePlay}>
                                     Play
                                 </Button>
                             </div>
